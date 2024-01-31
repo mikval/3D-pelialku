137248f53a340b0ca5e03b7dff6738b46c2851b1
mikval <e2203247@edu.vamk.fi>
2024-01-31 18:31:56 +0200

    uudelleenalustus. Pyörivä palikka poistettu.

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 import { initScene } from './scene.js';
-import { initCube } from './objekti.js';
 import { animate } from './anime.js';
 import { initControls } from './controls.js';
 
 
 const { scene, camera, renderer } = initScene();
-const cube = initCube(scene);
 const controls = initControls(camera, renderer);
 
-animate(scene, cube, camera, renderer);
+animate(scene, null, camera, renderer, controls); // Lähetä null sijasta objektia
